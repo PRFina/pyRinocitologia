78a2b8574c4f356c4206e605984d14592d2d8f8b
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-02-25 18:05:38 +0100

    Refactor classifier code in pythonic style using list comprehnsion, zip and enumerat and encapsulate code in functions. Add path dependencies trough configparser and config. file

--- src/classifier.py
+++ src/classifier.py
@@ -1,88 +1,84 @@
 # Import libraries and modules
 import cv2
 import os
+from os import path
 import numpy as np
 import shutil
+import configparser
 
 from keras.models import load_model
 
+# TODO: eplace with config. file
 
-
-def get_class_name(classNum):
+def get_class_path(classNum, config):
     # convert the cell-class number into its name string.
     if classNum == 0:
-        return APP_CONFIG["epiteliali_dir"]
+        return config["Paths"]["epiteliali_dir"]
     if classNum == 1:
-        return APP_CONFIG["neutrofili_dir"]
+        return config["Paths"]["neutrofili_dir"]
     if classNum == 2:
-        return APP_CONFIG["eosinofili_dir"]
+        return config["Paths"]["eosinofili_dir"]
     if classNum == 3:
-        return APP_CONFIG["mastcellule_dir"]
+        return config["Paths"]["mastcellule_dir"]
     if classNum == 4:
-        return APP_CONFIG["linfociti_dir"]
+        return config["Paths"]["linfociti_dir"]
     if classNum == 5:
-        return APP_CONFIG["mucipare_dir"]
+        return config["Paths"]["mucipare_dir"]
     if classNum == 6:
-        return APP_CONFIG["others_dir"]
+        return config["Paths"]["others_dir"]
 
 
-def load_data(data_directory):
+def load_resize_img(img_path):
+
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    return cv2.resize(img, (50, 50))
+
+
+def load_data(cell_directory):
+
     """
-    Loading images and labels from directory
-    :param data_directory:
-    :return: images, labels
+    Loading images and labels from cell directory
+    :param cell_directory: directory path with extracted cell images
+    :return: images, image'names
     """
-    directories = [d for d in os.listdir(data_directory)
-                   if os.path.isdir(os.path.join(data_directory, d))]
-    images = []
-    file_names = []
-    for d in directories:
-        # images are contained in a directory called cellule
-        if d == APP_CONFIG['cells_dir']:
-            label_directory = os.path.join(data_directory, d)
-            file_names = [os.path.join(label_directory, f)
-                      for f in os.listdir(label_directory)
-                      if f.endswith('.png') or f.endswith('.PNG')]
-            for f in file_names:
-                img = cv2.imread(f)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img = cv2.resize(img, (50, 50))
-                images.append(img)
+    file_names = [path.join(cell_directory, img_name) for img_name in os.listdir(cell_directory)
+                  if img_name.endswith('.png') or img_name.endswith('.PNG')]
+    images = [load_resize_img(img_name) for img_name in file_names]
 
     return images, file_names
 
 
-# load the trained model.
-model=load_model('my_model.h5')
-# load the best weights for the model.
-model.load_weights('weights.best.hdf5')
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
+    # load the trained model.
+    model = load_model(config["Models"]["classifier"])
+    model.load_weights(config["Models"]["classifier_weights"])
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
-# define the path of the folders where images are contained.
-path = os.getcwd()
+    # load cell images
+    images, file_names = load_data(config["Paths"]["cells_dir"])
 
-img_list, file_names = load_data(path)
+    # TODO: review the code below, use skimage functions to convert and optimize the code
+    # images to multi dimensional arrays
+    images = np.array(images)
+    images = images.astype("float32")
+    images /= 255
+    images = images.reshape(images.shape[0], 50, 50, 3)
 
-# images to multi dimensional arrays
-img_list = np.array(img_list)
-img_list = img_list.astype("float32")
-img_list /= 255
-img_list = img_list.reshape(img_list.shape[0], 50, 50, 3)
+    out_path = config["Paths"]["output_dir"]
 
-# define a counter for the file names...
-i = 0
-for img in img_list:
-    # for each image in the list
-    img = img.reshape((1,)+img.shape)
-    # add a dimension to the image
-    img_class = model.predict_classes(img)
+    for i, (img, img_name) in enumerate(zip(images, file_names)):
+        img = img.reshape((1,)+img.shape)
+        # add a dimension to the image
+        img_class = model.predict_classes(img)
 
-    # define the path of its folder-class (classify the image).
-    class_name = get_class_name(img_class)
-    class_path = os.path.join(path, APP_CONFIG['output_dir'], class_name)
+        # define the path of its folder-class (classify the image).
+        class_path = get_class_path(img_class, config)
 
-    # move it to the correct destination.
-    shutil.move(file_names[i], class_path)
-    print("Predict class for image {}: {}".format(file_names[i], class_name))
-    i += 1
+        # move it to the correct destination.
+        shutil.move(img_name, class_path)
+        print("Predict class for image {}: {}".format(file_names[i], class_path))
