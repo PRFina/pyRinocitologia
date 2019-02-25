0e9d4d31349f6c4aa1b204551d227064fce08b4f
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-02-25 19:06:14 +0100

    Update path in clean shell script

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 EXTENSION=".png"
+ASSETS_PATH="data/assets"
 
 case $1 in
 "cellule")
-    find assets/cellule -name "*$EXTENSION" -type f -delete
+    find $ASSETS_PATH/cellule -name "*$EXTENSION" -type f -delete
     echo "cellule directory cleaned"
     ;;
 "out")
-    find assets/out -name "*$EXTENSION" -type f -delete
+    find $ASSETS_PATH/out -name "*$EXTENSION" -type f -delete
     echo "out directory cleaned"
     ;;
-".")
-    find assets/cellule -name "*$EXTENSION" -type f -delete
-    find assets/out -name "*$EXTENSION" -type f -delete
+"*")
+    find $ASSETS_PATH -name "*$EXTENSION" -type f -delete
+    find $ASSETS_PATH -name "*$EXTENSION" -type f -delete
     echo "directories cleaned"
     ;;
 esac
