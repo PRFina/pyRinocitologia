67849045aa499054b684f5ae080532687d907cb0
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-02-27 21:37:35 +0100

    Add method to DataManager to retrieve cell class path

--- src/data_manager.py
+++ src/data_manager.py
@@ -10,13 +10,17 @@ class DataManager:
         self.input_dir = os.path.join(self.assets_dir, "input")
         self.cells_dir = os.path.join(self.assets_dir, "cells")
         self.out_dir = os.path.join(self.assets_dir, "out")
-        self._classes = ["eosinofili", "epiteliali", "linfociti", "mastcellule", "mucipare", "neutrofili", "others"]
-        self.classes_dir = []
         self._allowed_input_extensions = [".png", ".PNG", ".jpeg", ".JPEG", ".jpg", ".JPG"]
         self._allowed_output_extensions = [".png"]
 
-        for cell_class in self._classes:
-            self.classes_dir.append(os.path.join(self.out_dir, cell_class))
+        self._classes = {0: "epiteliali",
+                         1: "neutrofili",
+                         2: "eosinofili",
+                         3: "mastcellule",
+                         4: "linfociti",
+                         5: "mucipare",
+                         6: "others"
+                         }
 
     @staticmethod
     def get_file_by_extensions(path, allowed_extensions):
@@ -33,3 +37,6 @@ class DataManager:
 
     def get_output_extension(self):
         return self._allowed_output_extensions[0]
+
+    def get_cell_class_dir(self, class_index):
+        return os.path.join(self.out_dir, self._classes[class_index])
