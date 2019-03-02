04d7fddcdb5f01cf3d1ead61a459619db9af970f
Pio Raffaele Fina <pioraffaelefina@outlook.it>
2019-03-02 13:53:44 +0100

    Add extraction_steps named tuple as return value to detect_cells function

--- src/watershed_extraction.py
+++ src/watershed_extraction.py
@@ -17,14 +17,19 @@ from skimage import io
 from skimage.measure import regionprops
 from skimage.feature import peak_local_max
 from data_manager import DataManager
-
+from collections import  namedtuple
 
 LOW_THRESHOLD_SIZE = 1000
 HIGH_THRESHOLD_SIZE = 15000
 
 
-def detect_cells(image):
+def detect_cells(image, return_steps=False):
 
+    extraction_steps = namedtuple("ExtractionSteps", ["input", "meanshift",
+                                                      "grayscale", "binary",
+                                                      "dilation", "distance",
+                                                      "labels", "filtered_labels"
+                                                      ])
     # perform pyramid mean shift filtering
     # to aid the thresholding step
     shifted = cv2.pyrMeanShiftFiltering(image, 21, 51)
@@ -33,24 +38,24 @@ def detect_cells(image):
     # Otsu's thresholding
     # gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
     gray = cv2.cvtColor(shifted, cv2.COLOR_RGB2GRAY)
-    thresh = cv2.threshold(gray, 0, 255,
-                           cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    binary = cv2.threshold(gray, 0, 255,
+                              cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
     # morphological transformation
     selem = morphology.disk(5)
-    thresh = morphology.dilation(thresh, selem)
+    dilated = morphology.dilation(binary, selem)
 
     # compute the exact Euclidean distance from every binary
     # pixel to the nearest zero pixel, then find peaks in this
     # distance map
-    d = ndimage.distance_transform_edt(thresh)
-    local_max = peak_local_max(d, indices=False, min_distance=20,
-                               labels=thresh)
+    dist_map = ndimage.distance_transform_edt(dilated)
+    local_max = peak_local_max(dist_map, indices=False, min_distance=20,
+                               labels=dilated)
 
     # perform a connected component analysis on the local peaks,
     # using 8-connectivity, then apply the Watershed algorithm
     markers = ndimage.label(local_max, structure=np.ones((3, 3)))[0]
-    labels = morphology.watershed(-d, markers, mask=thresh)
+    labels = morphology.watershed(-dist_map, markers, mask=dilated)
     
     # Remove labels too small and too big
     filtered_labels = np.copy(labels)
@@ -64,16 +69,21 @@ def detect_cells(image):
     too_big_mask = too_big[labels]
     filtered_labels[too_big_mask] = 1
 
+    if return_steps:
+        return extraction_steps(input=image, meanshift=shifted,
+                             grayscale=gray, binary=binary,
+                             dilation=dilated, distance=dist_map,
+                             labels=labels, filtered_labels=filtered_labels)
     return filtered_labels
 
 
 def extract_cells(image, image_index, out_path):
 
-    filtered_labels = detect_cells(image)
+    filtered_labels = detect_cells(image, return_steps=True)
 
-    regions = regionprops(filtered_labels)
+    regions = regionprops(filtered_labels.filtered_labels)
     export_img_extension = data_manager.get_output_extension()
-    for i, region in enumerate(regions[1:]): #jump the first region (regions[0]) because is the entire image
+    for i, region in enumerate(regions[1:]):  # jump the first region (regions[0]) because is the entire image
 
         minr, minc, maxr, maxc = region.bbox
 
